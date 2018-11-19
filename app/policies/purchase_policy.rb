878aab67a97c33090941f5f3c295bf50f48c14e3
Nicolas Schroeder <nicolas.2000@hotmail.de>
2018-11-19 15:34:47 -0300

    controllers_policy

---
+++ app/policies/purchase_policy.rb
@@ -0,0 +1,15 @@
+class PurchasePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(user_id: current_user.id)
+    end
+  end
+
+  def new?
+    user.role == 'consumer'
+  end
+
+  def create?
+    new?
+  end
+end
