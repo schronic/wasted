9ce4b5165fb61679d6336567cb9bc29106997b78
Tyson Hazard <tysonhazard@gmail.com>
2018-11-19 19:28:20 -0300

    Add Item belongs_to :user in Item model to allow seed to assign user to Item.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,33 @@ Purchase.destroy_all
 puts 'Creating new items...'
 
 20.times do
-  user = User.create!({
+  user = User.new({
     email: Faker::Internet.email,
     password: 'wasted',
     username: Faker::Internet.username(8),
-    role: %w[consumer supplier].sample,
-    avatar_url: 'https://picsum.photos/100/100/?random'
+    role: %w[consumer supplier].sample
   })
+    user.remote_avatar_url_url = Cloudinary::Uploader.upload('https://picsum.photos/100/100/?random')['url']
+    user.save!
+
   rand(1..5).times do
-    item = Item.create!({
+    item = Item.new({
       name: Faker::Food.dish,
       description: Faker::Food.description,
       expiration: Faker::Date.between(2.days.ago, Date.today),
       price: rand(1..5),
       pickup_time: Faker::Date.forward(5),
-      picture: 'https://picsum.photos/200/300/?random',
-      quantity: rand(1..5)
+      quantity: rand(1..5),
+      user: user
     })
+    item.remote_picture_url = Cloudinary::Uploader.upload('https://picsum.photos/200/300/?random')['url']
+    item.save!
+
     rand(1..5).times do
       purchase = Purchase.create!({
         total_price: rand(1..30)
       })
+
       rand(1..5).times do
         reservation = Reservation.create!({
           item: item,
