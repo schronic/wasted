96d8ee7817d2e9708cbc8394133cd075fcfe9d5a
Nicolas Schroeder <nicolas.2000@hotmail.de>
2018-11-20 18:37:12 -0300

    ...

--- db/migrate/20181120144507_create_orders.rb
+++
@@ -1,8 +0,0 @@
-class CreateOrders < ActiveRecord::Migration[5.2]
-  def change
-    create_table :orders do |t|
-      t.integer     :total_price
-      t.references  :user, foreign_key: true
-    end
-  end
-end
