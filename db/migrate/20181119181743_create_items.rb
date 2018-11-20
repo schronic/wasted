4e9dc07efba3effb0bf171f9dbb1867a4a199e98
Tyson Hazard <tysonhazard@gmail.com>
2018-11-20 16:02:56 -0300

    Fix spacing in migrations.

--- db/migrate/20181119181743_create_items.rb
+++ db/migrate/20181119181743_create_items.rb
@@ -1,14 +1,14 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string :name
-      t.text :description
-      t.datetime :expiration, default: -> { 'CURRENT_TIMESTAMP' }
-      t.integer :price
-      t.datetime :pickup_time, default: -> { 'CURRENT_TIMESTAMP' }
-      t.string :picture
-      t.integer :quantity, default: 1
-      t.references :user, foreign_key: true
+      t.string      :name
+      t.text        :description
+      t.datetime    :expiration, default: -> { 'CURRENT_TIMESTAMP' }
+      t.integer     :price
+      t.datetime    :pickup_time, default: -> { 'CURRENT_TIMESTAMP' }
+      t.string      :picture
+      t.integer     :quantity, default: 1
+      t.references  :user, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20181120144507_create_orders.rb
+++ db/migrate/20181120144507_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.integer :total_price
-      t.references :user, foreign_key: true
+      t.integer     :total_price
+      t.references  :user, foreign_key: true
     end
   end
 end
