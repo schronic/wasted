7c4f2992ec8c958ac64c408d43f6570884e228a9
Nicolas Schroeder <nicolas.2000@hotmail.de>
2018-11-20 20:18:34 -0300

    code wrong

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_11_20_210217) do
 
   # These are extensions that must be enabled in order to support this database
@@ -45,6 +44,15 @@ ActiveRecord::Schema.define(version: 2018_11_20_210217) do
     t.string "amount_currency", default: "USD", null: false
     t.jsonb "payment"
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "purchased_items", force: :cascade do |t|
