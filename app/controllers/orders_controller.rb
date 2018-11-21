512385b52d9155b60773ab8bcf0347c6f553af9c
Tyson Hazard <tysonhazard@gmail.com>
2018-11-21 18:59:05 -0300

    Work on controllers for orders, reservations, and purchased_items for shopping cart.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: %i[new show]
   def index
+    @orders = policy_scope(Order).order(created_at: :desc)
   end
 
   def show
-    @order = current_user.orders.where(state: 'paid').find(params[:id])
+    authorize @order
   end
 
   def new
+    authorize @order
   end
 
   def create
@@ -14,5 +17,11 @@ class OrdersController < ApplicationController
     order = Order.create!(item_sku: item.sku, amount: item.price, state: 'pending', user: current_user)
 
     redirect_to new_order_payment_path(order)
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
   end
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -7,13 +7,17 @@ class ReservationsController < ApplicationController
     @reservations = policy_scope(Reservation).order(created_at: :desc)
     if user_signed_in?
       @purchased_item = PurchasedItem.new
-      # raise
+      @subtotal_price = 0
       @total_price = 0
       @total_items = 0
       @reservations.each do |reservation|
-        @total_price += reservation.item.price
+        @subtotal_price += reservation.item.price
         @total_items += 1
       end
+      percent = 0.05
+      @commission = @subtotal_price * percent
+      @total_price = @subtotal_price * (1 + percent)
+      @amount_cents = @total_price * 100
     else
       redirect_to reservations_error_path
     end
