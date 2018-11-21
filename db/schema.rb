21685b29b8d4937c61c175cb3be4f9c43dc49346
Astrid Bosch <boschschwarz@hotmail.com>
2018-11-21 15:19:24 -0300

    trying to fix essential problems

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2018_11_20_210217) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
   create_table "purchased_items", force: :cascade do |t|
     t.integer "item_purchase_price"
     t.integer "item_purchase_quantity"
