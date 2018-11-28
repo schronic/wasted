2284975dccba2f328908d4f9673bafdfa20c4959
Nicole Velasco <nicolevelasco455@gmail.com>
2018-11-28 20:51:27 -0300

    part of form

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -3,5 +3,17 @@ import 'select2';
 
 $('.select2').select2();
 
+const price = document.getElementById("item_price")
+price.value = ""
+price.placeholder = "Price"
+
+const quantity = document.getElementById("item_quantity")
+quantity.value = ""
+quantity.placeholder = "Quantity"
+
+const types = document.getElementById("types")
+types.value = ""
+types.placeholder = "Types"
+
 // Requiring CSS! Path is relative to ./node_modules
 import 'select2/dist/css/select2.css';
