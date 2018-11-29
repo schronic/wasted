6ec0e28aa13847bcc2f4f0e21f9b89b69b0494ba
Nicole Velasco <nicolevelasco455@gmail.com>
2018-11-29 11:41:18 -0300

    all forms styles

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import 'select2';
 
-$('.select2').select2();
+$('.select2').select2({
+  placeholder: "Types"
+});
 
 const price = document.getElementById("item_price")
 price.value = ""
@@ -11,9 +13,15 @@ const quantity = document.getElementById("item_quantity")
 quantity.value = ""
 quantity.placeholder = "Quantity"
 
-const types = document.getElementById("types")
-types.value = ""
-types.placeholder = "Types"
+const start = document.getElementById("start_date")
+start.value = ""
+start.label = ""
+start.placeholder = "Pick up time"
+
+const end = document.getElementById("end_date")
+end.value = ""
+end.placeholder = "Expiration"
+
 
 // Requiring CSS! Path is relative to ./node_modules
 import 'select2/dist/css/select2.css';
