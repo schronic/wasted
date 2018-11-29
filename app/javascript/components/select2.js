de461473be335508e68815cc87ec24f25b1f5337
Nicole Velasco <nicolevelasco455@gmail.com>
2018-11-29 19:41:05 -0300

    validation, home pciture, 404 picture, 500 picture and javscript typing

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -5,22 +5,32 @@ $('.select2').select2({
   placeholder: "Types"
 });
 
-const price = document.getElementById("item_price")
+if (document.getElementById("item_price")) {
+ const price = document.getElementById("item_price")
 price.value = ""
 price.placeholder = "Price"
+}
 
-const quantity = document.getElementById("item_quantity")
+if (document.getElementById("item_quantity")) {
+  const quantity = document.getElementById("item_quantity")
 quantity.value = ""
 quantity.placeholder = "Quantity"
+}
 
-const start = document.getElementById("start_date")
+
+if (document.getElementById("start_date")) {
+  const start = document.getElementById("start_date")
 start.value = ""
 start.label = ""
 start.placeholder = "Pick up time"
+}
 
-const end = document.getElementById("end_date")
+if (document.getElementById("end_date")) {
+  const end = document.getElementById("end_date")
 end.value = ""
 end.placeholder = "Expiration"
+}
+
 
 
 // Requiring CSS! Path is relative to ./node_modules
