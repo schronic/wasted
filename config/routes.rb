2591e211844292e54c52a322485a1d9cd123a478
Tyson Hazard <tysonhazard@gmail.com>
2018-11-30 04:20:59 -0300

    Post and patch for plus icon depending on whether reservation already exists for item.

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
   get    '/cart',             to: 'reservations#index',   as: 'cart'
   post   '/reservations',     to: 'reservations#create',  as: 'create_reservation'
   get    '/cart/:id',         to: 'reservations#show',    as: 'reservation'
-  patch  '/cart/:id', to: 'reservations#update',  as: 'patch_reservation'
+  patch  '/cart/:id',         to: 'reservations#update',  as: 'patch_reservation'
+  patch  '/reservations',     to: 'reservations#update'
   delete '/cart/:id',         to: 'reservations#destroy', as: 'delete_reservation'
 
   resources :orders, only: [:new, :index, :show, :create, :destroy] do
